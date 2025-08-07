7d442f182b2a55cd89eb60e781a7092a9275797a
Leen <Leena.kraytem@gmail.com>
2025-08-07 00:24:00 -0400

    no this is the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import Missions from './pages/Missions';
 
+import Navbar from './components/Navbar';
+
 
 import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/missions" element={<Missions />}/>
