e75d2c34fe98741d1bae859791a6c36eb1884ec5
Leen <Leena.kraytem@gmail.com>
2025-09-07 19:32:06 -0400

    storyline change 1

--- src/pages/Phishing/PhishingLesson.jsx
+++ src/pages/Phishing/PhishingLesson.jsx
@@ -14,18 +14,18 @@ function PhishingLesson() {
         <div className="min-h-screen flex flex-col items-center pt-50 bg-white">
             {!showVideo ? (
                 <>
-                    <h1 className="text-4xl font-bold mb-6">Are You Ready?</h1>
+                    <h1 className="text-4xl font-bold mb-6">You see what she missed?</h1>
                     <button
                         className="w-96 h-28 text-3xl bg-slate-300 text-white rounded-2xl shadow hover:bg-red-200 transition font-bold flex items-center justify-center mb-8"
                         onClick={() => setShowVideo(true)}
                     >
-                        Yes!
+                        Saw it from a mile away 🚩
                     </button>
                     <button
                         className="w-96 h-28 text-3xl bg-slate-300 text-white rounded-2xl shadow hover:bg-red-200 transition font-bold flex items-center justify-center mb-8"
                         onClick={() => setShowVideo(true)}
                     >
-                        Yes!!
+                        Wait… what??
                     </button>
                 </>
             ) : (
